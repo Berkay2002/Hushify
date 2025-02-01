4cdcd890471d089866d96ca6b338a4ff7bedadaf
Berkay Orhan <BerkayOrhan@hotmail.se>
2025-02-01 04:15:03 +0100

    S

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -4,14 +4,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, // This one is correct
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, // Make sure this variable is set
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL, // Notice: your env variable is NEXT_PUBLIC_DATABASE_URL
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID, // Updated: use NEXT_PUBLIC_PROJECT_ID instead of NEXT_PUBLIC_FIREBASE_PROJECT_ID
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET, // Should match your env variable
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
